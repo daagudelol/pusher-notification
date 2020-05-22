e073c0ce7ba0430546e0ce418e90aaec20349fed
diego alejandro A Lugo <Diego Agudelo>
2020-05-22 08:51:18 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Pusher from 'pusher-js';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notifications';
+  totalProducts$: Observable<number>;
+  public notifications: number = 0;
+  public message;
+  
+
+  constructor() { }
+
+  
+
+  ngOnInit() {
+      this.pusherListen();
+  }
+
+  private pusherListen() {
+      var pusher = new Pusher('xxxxxxxxxxxxxxxxx', {
+          cluster: 'us2',
+          forceTLS: true,
+      });
+      var channel = pusher.subscribe('branches_channel');
+      channel.bind_global(function(event, data) {
+        this.message = data.user;
+          console.log('Received my-event with message: ' + data.user);
+          if (data.user) {
+              this.notifications++;
+          }
+        });
+  }
+
+  view() {
+      this.notifications = 0;
+  }
 }
